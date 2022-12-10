0e36b6456ebacfb8faa28cfa45e4dac533bd8d7c
rubayatrifat <rubayatislam17@gmail.com>
2022-12-10 17:16:14 +0600

    Js Practishes Here .

---
+++ main.js
@@ -0,0 +1,11 @@
+// Oporetors
+
+console.log(5 + 5) //plus
+console.log(5 - 3) // minies
+console.log(5 * 2) //into
+console.log(10 / 5) // Devite
+console.log(5 % 2) //modulas
+console.log(5 == 3) // bolean deta type or double eculal
+console.log(5 == 5) // bolean deta type or double eculal
+console.log(5 == '5') // bolean deta type or double eculal
+console.log(5 === '5') // bolean deta type or triple eculal
